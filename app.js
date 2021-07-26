d0e400be23d7d3609fc44b4f8dc478ccd52d061c
Waqar-107 <waqar.hassan866@gmail.com>
2021-07-26 21:53:20 +0600

    bot is replying to messages

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ client.on('ready', () => {
 	console.log('Logged in as ' + client.user.username);
 	console.log('------');
 });
+
+client.on('message', (msg) => {
+	if (msg.content === 'yo') msg.reply('kukhur!');
+	else if (msg.content.includes('biye') && msg.author.id != client.user.id)
+		msg.reply('vai, biyer kotha bole pera dish na toh');
+});
